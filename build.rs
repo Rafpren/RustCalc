a7b8e81cf75c78eedcca77ce29172d5afa09211a
Raffaele Prencipe <268448116+Rafpren@users.noreply.github.com>
2026-03-23 21:05:47 +0100

    Sauvegarde intermédiaire avant rebase

--- build.rs
+++ build.rs
@@ -1,11 +1,8 @@
 fn main() {
-    // Ce script ne doit s'exécuter que si l'on compile pour Windows
     #[cfg(target_os = "windows")]
     {
         let mut res = winres::WindowsResource::new();
-        // Spécifie le chemin vers ton fichier .ico
         res.set_icon("icon.ico");
-        // Tu peux aussi ajouter des métadonnées visibles dans les propriétés du fichier
         res.set("ProductName", "RustCalc");
         res.set("CompanyName", "Rafpren");
         res.compile().unwrap();
